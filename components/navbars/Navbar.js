54c730c64a1b62b569a1e11c83b18385b8489f62
Xiaoqing-Lin <57362207+Xiaoqing-Lin@users.noreply.github.com>
2020-06-22 17:25:08 -0400

    update: navbar hover template complete

--- components/navbars/Navbar.js
+++ components/navbars/Navbar.js
@@ -1,23 +1,15 @@
-import {} from 'react';
 import {connect} from 'react-redux';
 
-import styles from './Navbar.module.css';
 import NavbarAnon from './NavbarAnon';
 import NavbarUser from './NavbarUser';
 
 const Navbar = ({status}) => {
   const navbar = () => {
-    switch (status) {
-      case 'logged in': {
-        return <NavbarAnon />;
-      }
-      case 'logged out': {
-        return <NavbarUser />;
-      }
-      default:
-        return <NavbarUser />;
-        // return <NavbarAnon />;
+    const status = 'logged in';
+    if (status === 'logged in') {
+      return <NavbarUser />;
     }
+    return <NavbarAnon />;
   };
 
   return <div>{navbar()}</div>;
